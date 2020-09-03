c9f4f92e47b9f61582dbc9bd73771d68734ee9a9
Adam <adam.vernon@my.jcu.edu.au>
2020-09-03 20:33:59 +1000

    Add warm up task

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,13 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers + [6, 5, 3])
+"""
+numbers[0] 3
+numbers[-1] 2
+numbers[3] 4
+numbers[:-1] [3, 1, 4, 1, 5, 9]
+numbers[3:4] [3, 1, 4, 1]
+5 in numbers true
+7 in numbers false
+"3" in numbers false
+numbers + [6, 5, 3] [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+"""
